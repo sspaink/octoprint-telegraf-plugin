00239c7495420b810eb0c34a4ca46a89f503568a
Bas <3441183+BattleBas@users.noreply.github.com>
2020-09-14 21:25:01 -0500

    sample

--- cmd/main.go
+++ cmd/main.go
@@ -6,32 +6,15 @@ import (
 	"os"
 	"time"
 
-	// TODO: import your plugins
 	_ "github.com/battlebas/octoprint-telegraf-plugin/plugins/inputs/octoprint"
-
 	"github.com/influxdata/telegraf/plugins/common/shim"
 )
 
-var pollInterval = flag.Duration("poll_interval", 1*time.Second, "how often to send metrics")
+var pollInterval = flag.Duration("poll_interval", 10*time.Second, "how often to send metrics")
 var pollIntervalDisabled = flag.Bool("poll_interval_disabled", false, "how often to send metrics")
 var configFile = flag.String("config", "", "path to the config file for this plugin")
 var err error
 
-// This is designed to be simple; Just change the import above and you're good.
-//
-// However, if you want to do all your config in code, you can like so:
-//
-// // initialize your plugin with any settngs you want
-// myInput := &mypluginname.MyPlugin{
-// 	DefaultSettingHere: 3,
-// }
-//
-// shim := shim.New()
-//
-// shim.AddInput(myInput)
-//
-// // now the shim.Run() call as below.
-//
 func main() {
 	// parse command line options
 	flag.Parse()
